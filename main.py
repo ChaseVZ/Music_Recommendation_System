8bc441b77e72baa02e4fdd743aa7a1bf75829bf1
sumukhipandey <sumukhi.a.pandey@gmail.com>
2022-04-30 17:14:01 -0700

    Test Tensor Flow

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import tensorflow as tf
  
 # import tensorflow_datasets as tfds
 # import tensorflow_recommenders as tfrs
+tf.compat.v1.disable_eager_execution()
+node1 = tf.constant(32.0)
+node2 = tf.constant(32.0)
+node3 = node1 + node2
 
+# create tensorflow session object
+sess = tf.compat.v1.Session()
 
-def main():
-    print("Hello World")
+# evaluating node3 and printing the result
+print("Sum of node1 and node2 is:", sess.run(node3))
 
-
-if __name__ == "__main__":
-    main()
-
+# closing the session
+sess.close()
